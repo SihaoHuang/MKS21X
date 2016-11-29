80edc2b3e0eae16ec1e7bfd319054b2b08d9c2db
Sihao Huang <astronomywong@gmail.com>
2016-11-29 18:37:20 -0500

    Added Library project

---
+++ Library/Book.java
@@ -0,0 +1,47 @@
+public class Book{
+
+  private String author;
+  private String title;
+  private String ISBN;
+
+  public Book(){
+    author = "";
+    title = "";
+    ISBN = "";
+  }
+
+  public Book(String theAuthor, String theTitle, String theISBN){
+    author = theAuthor;
+    title = theTitle;
+    ISBN = theISBN;
+  }
+
+  public String toString(){
+    return "The author is " + author + ", the title is " + title + "and the ISBN is " + ISBN + ".";
+  }
+
+  public void setAuthor(String newAuthor){
+    author = newAuthor;
+  }
+
+  public void setTitle(String newTitle){
+    title = newTitle;
+  }
+
+  public void setISBN(String newISBN){
+    ISBN = newISBN;
+  }
+
+  public String getAuthor(){
+    return author;
+  }
+
+  public String getTitle(){
+    return title;
+  }
+
+  public String getISBN(){
+    return ISBN;
+  }
+
+}
